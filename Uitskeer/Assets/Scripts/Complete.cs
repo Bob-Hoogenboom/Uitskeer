d74cd77bba6f7246cf16bc7da6c02f4aea14ad80
Bob-Hoogenboom <bhoogenboom23@gmail.com>
2024-11-07 15:31:58 +0100

    RadioHead
    StreetSpirit

---
+++ Uitskeer/Assets/Scripts/Complete.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class Complete : MonoBehaviour
+{
+    public Animator anim;
+
+    [SerializeField] private float transitionTime = 1f;
+
+    public void TransitionToEndScreen()
+    {
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade()
+    {
+        anim.SetTrigger("FadeOut");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        anim.SetTrigger("FadeIn");
+    }
+}
